766736dcdfa7f35a4bfff76c8e8f0432e73e1c5c
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-01-22 12:49:00 +0000

    css changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import { useState, useEffect } from 'react';
 import './App.css';
 import SearchBar from "./components/RecipeCard";
 import RecipeCard from"./components/RecipeCard";
 
-const apiUrl ="www.themealdb.com/api/json/v1/1/search.php?s=";
+const apiUrl ="https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
@@ -13,12 +12,32 @@ function App() {
 
 const searchRecipes = async () => {
   setIsLoading(true);
-  const url = apiUrl + query
+  const url = apiUrl + query;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+  setRecipes(data.meals);
+  setIsLoading(false);
 };
+
+useEffect(() => {
+  searchRecipes();
+
+}, []);
 
   return (
     <div className="container">
       <h2>Our Recipe App</h2>
+      <div className='recipes'>
+        {recipes ? recipes.map(recipe => (
+          <RecipeCard 
+          key={recipe.idMeal}
+          recipe={recipe}
+          />
+
+        )): "No Recipes!"}
+
+      </div>
 
     </div>
   );
